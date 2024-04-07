b991a8c19ba4599c120bcfbc9237e7782b9db14a
warman <159046867+runreal-warman@users.noreply.github.com>
2024-04-07 13:57:57 -0400

    ci: test cloning p4 into subdir

--- python/buildkite.py
+++ python/buildkite.py
@@ -57,7 +57,7 @@ def get_config():
     if "BUILDKITE_PLUGIN_PERFORCE_ROOT" in os.environ and not __LOCAL_RUN__:
         raise Exception("Custom P4 root is for use in unit tests only")
     conf["root"] = os.environ.get("BUILDKITE_PLUGIN_PERFORCE_ROOT") or os.environ.get(
-        "BUILDKITE_BUILD_CHECKOUT_PATH"
+        "BUILDKITE_BUILD_CHECKOUT_PATH/p4"
     )
 
     # Coerce view into pairs of [depot client] paths
